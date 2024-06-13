977e55c163e04ae89703f9a506f55b47a81c2aab
Sebastien <sebastien.guerrero@insa-lyon.fr>
2024-06-13 14:29:21 +0200

    [dev] configuration file upraded: instr+modules

--- Keithley/pymodaq_plugins_keithley/src/pymodaq_plugins_keithley/__init__.py
+++ Keithley/pymodaq_plugins_keithley/src/pymodaq_plugins_keithley/__init__.py
@@ -1,10 +1,13 @@
+import os
 from pathlib import Path
 from pymodaq.utils.logger import set_logger  # to be imported by other modules.
 
-from pymodaq_plugins_keithley.utils import Config_k2700, Config_k2701
-config_k2700 = Config_k2700()
-config_k2701 = Config_k2701()
+import pymodaq_plugins_keithley.utils as utils
+
+config_keithley = utils.Config()
+toml_modules = [f for f in os.listdir('resources/') if "module" in f and ".toml" in f]
+for file in toml_modules:
+    exec("config_k" + str(file[-9:-5]) + " = " + "utils.Config_k" + str(file[-9:-5]) + "()")
 
 with open(str(Path(__file__).parent.joinpath('resources/VERSION')), 'r') as fvers:
     __version__ = fvers.read().strip()
-
--- Keithley/pymodaq_plugins_keithley/src/pymodaq_plugins_keithley/utils.py
+++ Keithley/pymodaq_plugins_keithley/src/pymodaq_plugins_keithley/utils.py
@@ -4,17 +4,21 @@ Created the 31/08/2023
 
 @author: Sebastien Weber
 """
+import os
 from pathlib import Path
-
 from pymodaq.utils.config import BaseConfig, USER
 
-
-class Config_k2700(BaseConfig):
+class Config(BaseConfig):
     """Main class to deal with configuration values for this plugin"""
-    config_template_path = Path(__file__).parent.joinpath('resources/config_k2700_module7706.toml')
-    config_name = f"config_k2700_module7706"
+    config_template_path = Path(__file__).parent.joinpath(f"resources/config_{__package__.split('pymodaq_plugins_')[1]}.toml")
+    config_name = f"config_{__package__.split('pymodaq_plugins_')[1]}"
 
-class Config_k2701(BaseConfig):
-    """Main class to deal with configuration values for this plugin"""
-    config_template_path = Path(__file__).parent.joinpath('resources/config_k2701_module7708.toml')
-    config_name = f"config_k2701_module7708"
+toml_modules = [f for f in os.listdir('resources/') if "module" in f and ".toml" in f]
+print("--- Modules conf files:",toml_modules)
+
+for file in toml_modules:
+    class ConfigModule(BaseConfig):
+        config_template_path = Path(__file__).parent.joinpath("resources/config_module" + str(file[-9:-5]) + ".toml")
+        config_name = f"config_module" + str(file[-9:-5])
+
+    exec("class Config_k" + str(file[-9:-5]) + "(ConfigModule): pass")
